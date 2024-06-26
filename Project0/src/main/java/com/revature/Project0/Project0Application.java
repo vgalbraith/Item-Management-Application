12f4fa1effb71005cd0a8635b5f849ce52618e78
Victor Galbraith <victorg87@gmail.com>
2024-06-26 16:41:47 -0700

    Spring Initializr for Project0

---
+++ Project0/src/main/java/com/revature/Project0/Project0Application.java
@@ -0,0 +1,11 @@
+package com.revature.Project_0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project0Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Project0Application.class, args);
+    }
+}
