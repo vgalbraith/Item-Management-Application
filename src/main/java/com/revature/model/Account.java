1af0551eba2c60ba96017d10bb75b193c89f5874
Victor Galbraith <victorg87@gmail.com>
2024-09-10 08:30:08 -0700

    Updated models to use simple record definitions

--- src/main/java/com/revature/model/Account.java
+++ src/main/java/com/revature/model/Account.java
@@ -1,77 +1,78 @@
 package com.revature.model;
 
-import jakarta.persistence.*;
+import org.springframework.data.annotation.Id;
 
-import java.util.Objects;
-
-@Entity
-@Table(name = "Accounts")
-public class Account {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(updatable = false)
-    private Integer account_id;
-
-    @Column(nullable = false, unique = true)
-    private String username;
-
-    @Column(columnDefinition = "smallint CHECK (password>=4)")
-    private String password;
-
-    public Account() {
-    }
-
-    public Account(Integer account_id, String username, String password) {
-        this.account_id = account_id;
-        this.username = username;
-        this.password = password;
-    }
-
-    public Integer getAccount_id() {
-        return account_id;
-    }
-
-    public void setAccount_id(Integer account_id) {
-        this.account_id = account_id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Account account = (Account) o;
-        return Objects.equals(account_id, account.account_id) && Objects.equals(username, account.username) && Objects.equals(password, account.password);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(account_id, username, password);
-    }
-
-    @Override
-    public String toString() {
-        return "Account{" +
-                "account_id=" + account_id +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
-
+public record Account(@Id Integer account_id, String username, String password) {
 }
+
+//@Entity
+//@Table(name = "Accounts")
+//public class Account {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(updatable = false)
+//    private Integer account_id;
+//
+//    @Column(nullable = false, unique = true)
+//    private String username;
+//
+//    @Column(columnDefinition = "smallint CHECK (password>=4)")
+//    private String password;
+//
+//    public Account() {
+//    }
+//
+//    public Account(Integer account_id, String username, String password) {
+//        this.account_id = account_id;
+//        this.username = username;
+//        this.password = password;
+//    }
+//
+//    public Integer getAccount_id() {
+//        return account_id;
+//    }
+//
+//    public void setAccount_id(Integer account_id) {
+//        this.account_id = account_id;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        Account account = (Account) o;
+//        return Objects.equals(account_id, account.account_id) && Objects.equals(username, account.username) && Objects.equals(password, account.password);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(account_id, username, password);
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Account{" +
+//                "account_id=" + account_id +
+//                ", username='" + username + '\'' +
+//                ", password='" + password + '\'' +
+//                '}';
+//    }
+//
+//}
--- src/main/java/com/revature/model/VideoGame.java
+++ src/main/java/com/revature/model/VideoGame.java
@@ -1,87 +1,88 @@
 package com.revature.model;
 
-import jakarta.persistence.*;
+import org.springframework.data.annotation.Id;
 
-import java.util.Objects;
-
-@Entity
-@Table(name = "Video_games")
-public class VideoGame {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(updatable = false)
-    private Integer game_id;
-
-    @Column(nullable = false)
-    private String title;
-
-    private String platform;
-
-    private Integer owned_by;
-
-    public VideoGame() {
-    }
-
-    public VideoGame(Integer game_id, String title, String platform, Integer owned_by) {
-        this.game_id = game_id;
-        this.title = title;
-        this.platform = platform;
-        this.owned_by = owned_by;
-    }
-
-    public Integer getGame_id() {
-        return game_id;
-    }
-
-    public void setGame_id(Integer game_id) {
-        this.game_id = game_id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getPlatform() {
-        return platform;
-    }
-
-    public void setPlatform(String platform) {
-        this.platform = platform;
-    }
-
-    public Integer getOwned_by() {
-        return owned_by;
-    }
-
-    public void setOwned_by(Integer owned_by) {
-        this.owned_by = owned_by;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        VideoGame videoGame = (VideoGame) o;
-        return Objects.equals(game_id, videoGame.game_id) && Objects.equals(title, videoGame.title) && Objects.equals(platform, videoGame.platform) && Objects.equals(owned_by, videoGame.owned_by);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(game_id, title, platform, owned_by);
-    }
-
-    @Override
-    public String toString() {
-        return "VideoGame{" +
-                "game_id=" + game_id +
-                ", title='" + title + '\'' +
-                ", platform='" + platform + '\'' +
-                ", owned_by=" + owned_by +
-                '}';
-    }
+public record VideoGame(@Id Integer game_id, String title, String platform, Integer owned_by) {
 }
+
+//@Entity
+//@Table(name = "Video_games")
+//public class VideoGame {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(updatable = false)
+//    private Integer game_id;
+//
+//    @Column(nullable = false)
+//    private String title;
+//
+//    private String platform;
+//
+//    private Integer owned_by;
+//
+//    public VideoGame() {
+//    }
+//
+//    public VideoGame(Integer game_id, String title, String platform, Integer owned_by) {
+//        this.game_id = game_id;
+//        this.title = title;
+//        this.platform = platform;
+//        this.owned_by = owned_by;
+//    }
+//
+//    public Integer getGame_id() {
+//        return game_id;
+//    }
+//
+//    public void setGame_id(Integer game_id) {
+//        this.game_id = game_id;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getPlatform() {
+//        return platform;
+//    }
+//
+//    public void setPlatform(String platform) {
+//        this.platform = platform;
+//    }
+//
+//    public Integer getOwned_by() {
+//        return owned_by;
+//    }
+//
+//    public void setOwned_by(Integer owned_by) {
+//        this.owned_by = owned_by;
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        VideoGame videoGame = (VideoGame) o;
+//        return Objects.equals(game_id, videoGame.game_id) && Objects.equals(title, videoGame.title) && Objects.equals(platform, videoGame.platform) && Objects.equals(owned_by, videoGame.owned_by);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(game_id, title, platform, owned_by);
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "VideoGame{" +
+//                "game_id=" + game_id +
+//                ", title='" + title + '\'' +
+//                ", platform='" + platform + '\'' +
+//                ", owned_by=" + owned_by +
+//                '}';
+//    }
+//}
